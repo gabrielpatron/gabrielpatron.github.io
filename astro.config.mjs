bf35fd87bd3fef5c4fc5846004860d2d3361c292
gabrielpatron <97416056+gabrielpatron@users.noreply.github.com>
2026-04-15 11:23:09 +0100

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,10 @@
-import { defineConfig } from 'astro/config'
+// @ts-check
+import { defineConfig } from "astro/config";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  site: 'https://gabrielpatron.github.io',
-})
+  site: "https://gabrielpatron.github.io",
+  vite: {
+    plugins: [tailwindcss()],
+  },
+});
