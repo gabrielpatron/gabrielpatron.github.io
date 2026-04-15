bd74fa093b6e9ab589ccc09de812740c3996ecd0
gabrielpatron <97416056+gabrielpatron@users.noreply.github.com>
2026-04-15 11:37:19 +0100

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,13 +1,11 @@
 export const siteConfig = {
   name: "Process Intelligence Laboratory",
-  title: "@University of Toronto",
+  title: "@University of Toronto Department of Chemical Engineering and Applied Chemistry",
   description: "Portfolio website of Ryan Fitzgerald",
-  accentColor: "#DDF097",
+  accentColor: "#B5ED74",
   social: {
-    email: "your-email@example.com",
+    email: "gabriel.patron17@imperial.ac.uk",
     linkedin: "https://linkedin.com/in/yourprofile",
-    twitter: "https://x.com/rfitzio",
-    github: "https://github.com/RyanFitzgerald",
   },
   aboutMe:
     "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rem quos asperiores nihil consequatur tempore cupiditate architecto natus commodi corrupti quas quasi facere est, dignissimos odit nam veniam sapiente ut, vitae eligendi ipsum dolor, nostrum ullam impedit! Corrupti ratione mollitia temporibus necessitatibus, consectetur reiciendis recusandae id, dolorum quaerat, vero pariatur. Ratione!",
