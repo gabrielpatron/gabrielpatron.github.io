4fd58e2a1a9e70e65922422d0347337b444b38f6
gabrielpatron <97416056+gabrielpatron@users.noreply.github.com>
2026-04-16 14:42:59 +0100

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -128,7 +128,7 @@ joinUs: {
   sections: [
     {
       heading: "Graduate Students",
-      text: "We are looking for up to two graduate students (PhD, MASc) to join our group in Fall 2026. Prospective students with a strong interest in computation, machine learning, and control should to apply to the Chemical Engineering & Applied Chemistry graduate studies program. Please reach out to Prof. Patrón if you are planning to apply and are interested in joining our group! \n More information for prospective graduate students: https://chem-eng.utoronto.ca/graduate-studies/applications-and-admissions/ ",
+      text: "We are looking for up to two graduate students (PhD, MASc) to join our group in Fall 2026. Prospective students with a strong interest in computation, machine learning, and control should to apply to the Chemical Engineering & Applied Chemistry graduate studies program. Please reach out to Prof. Patrón if you are planning to apply and are interested in joining our group! \n \n More information for prospective graduate students: https://chem-eng.utoronto.ca/graduate-studies/applications-and-admissions/ ",
     },
     {
       heading: "Undergraduate Students",
@@ -140,7 +140,7 @@ joinUs: {
     },
     {
       heading: "Collaborators",
-      text: "We are always open to collaborations with academic, industry, and public-sector partners. We are actively looking to develop computational methods with applications in the chemical, energy, and financial industries. Please reach out to Prof. Patrón to explore potential collaborations.. ",
+      text: "We are always open to collaborations with academic, industry, and public-sector partners. We are actively looking to develop computational methods with applications in the chemical, energy, and financial industries. Please reach out to Prof. Patrón to explore potential collaborations. ",
     },
   ],
 },
