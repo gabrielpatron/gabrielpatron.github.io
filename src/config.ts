61e1b697d75412bc7f5b2fd38a2c3c73d95f3341
gabrielpatron <patrongabriel@gmail.com>
2026-09-22 11:35:29 -0400

    bala

--- src/config.ts
+++ src/config.ts
@@ -187,6 +187,18 @@ export const siteConfig = {
     },
   },
   {
+    name: "Bala Murali",
+    role: "Undergraduate Thesis Student",
+    bio: "",
+    summary: "BASc, Chemical Engineering, University of Toronto \n \n Kevin aims to learn interpretable decision rules for capacity expansion of energy systems.",
+    image: "/images/bala_murali.jpg",
+    hoverImage: "/images/bala_murali_alt.png",
+    email: "balakannan.murali@mail.utoronto.ca",
+    links: {
+      linkedin: "https://www.linkedin.com/in/bala-kannan-m-61078a258/",
+    },
+  },
+  {
     name: "Kevin Gan",
     role: "Undergraduate Student",
     bio: "",
